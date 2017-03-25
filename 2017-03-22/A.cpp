512d3df22aa90dc96c6575a440baab9ca78fcd1b
gzgreg <gzgregoryzhang@gmail.com>
2017-03-25 11:47:22 -0700

    Codeforces 397

---
+++ 2017-03-22/A.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+
+int main(){
+	ios::sync_with_stdio(0);
+	
+	int n; cin >> n;
+
+	cout << (n%2 ? "contest" : "home") << endl;
+
+	return 0;
+}
