a9942379eaaa5fd549a48495fa0a5e2446c1ad9a
gzgreg <gzgregoryzhang@gmail.com>
2017-11-22 18:33:36 -0800

    Fast IO

---
+++ magic-io.cpp
@@ -0,0 +1,14 @@
+inline char get(void) {
+	static char buf[100000], *S = buf, *T = buf;
+	if (S == T) {
+		T = (S = buf) + fread(buf, 1, 100000, stdin);
+		if (S == T) return EOF;
+	}
+	return *S++;
+}
+inline void read(int &x) {
+	static char c; x = 0; int sgn = 0;
+	for (c = get(); c < '0' || c > '9'; c = get()) if (c == '-') sgn = 1;
+	for (; c >= '0' && c <= '9'; c = get()) x = x * 10 + c - '0';
+	if (sgn) x = -x;
+}
